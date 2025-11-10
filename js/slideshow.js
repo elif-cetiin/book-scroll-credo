e7578d9f8fda6b36e7d7c1f3b71dc8544bff7a02
Elif Cetin <cetinel0707@gmail.com>
2025-11-10 15:08:27 -0600

    slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,40 +1,73 @@
+/**
+ * ---------------------------------------------------------
+ * Book Scroll Slideshow Script
+ * ---------------------------------------------------------
+ * Author: Elif Cetin
+ * Institution: University Libraries - University of Louisiana at Lafayette
+ *
+ * Description:
+ *  - Automatically scrolls book covers horizontally
+ *  - Lightweight and dependency minimal (only requires jQuery)
+ *  - Designed for embedding into LibGuides
+ *
+ * Features:
+ *  ✅ Auto-scrolling
+ *  ✅ Hover to pause
+ *  ✅ Seamless looping carousel effect
+ *  ✅ Responsive width calculation based on image widths
+ *
+ * Usage:
+ *  - Include slideshow.js and styles.css in index.html
+ *  - Ensure HTML structure:
+ *        <div class="slideshow"><ul><li><img></li> ...</ul></div>
+ *
+ * ---------------------------------------------------------
+ */
+
 $(document).ready(function ($) {
 
-  function updateWidth() {
-    let totalWidth = 0;
-    $('.slideshow ul li').each(function () {
-      totalWidth += $(this).outerWidth(true);
-    });
-    $('.slideshow ul').css('width', totalWidth + 'px');
-  }
+    /* Calculates total width of all list items based on image size */
+    function updateWidth() {
+        let totalWidth = 0;
 
-  updateWidth();
+        $('.slideshow ul li').each(function () {
+            totalWidth += $(this).outerWidth(true);
+        });
 
-  function slideshow() {
-    const firstItem = $('.slideshow ul li').first();
-    const itemWidth = firstItem.outerWidth(true);
-
-    $('.slideshow ul').animate(
-      { marginLeft: '-' + itemWidth + 'px' },
-      1000,
-      function () {
-        $(this).css('marginLeft', '0');
-        firstItem.appendTo('.slideshow ul');
-      }
-    );
-  }
-
-  let interval = setInterval(slideshow, 3000);
-
-  $('.slideshow').hover(
-    function () {
-      clearInterval(interval);
-    },
-    function () {
-      interval = setInterval(slideshow, 3000);
+        $('.slideshow ul').css('width', totalWidth + 'px');
     }
-  );
 
-  $(window).on('resize', updateWidth);
+    updateWidth();
 
+    /* Main slideshow animation loop */
+    function slideshow() {
+        const firstItem = $('.slideshow ul li').first();
+        const itemWidth = firstItem.outerWidth(true);
+
+        $('.slideshow ul').animate(
+            { marginLeft: '-' + itemWidth + 'px' },
+            1000,
+            function () {
+                $(this).css('marginLeft', '0');
+                firstItem.appendTo('.slideshow ul');
+            }
+        );
+    }
+
+    /* Auto-scroll interval */
+    let interval = setInterval(slideshow, 3000);
+
+    /* Pause animation when users hover */
+    $('.slideshow').hover(
+        function () {
+            clearInterval(interval);
+        },
+        function () {
+            interval = setInterval(slideshow, 3000);
+        }
+    );
+
+    /* Recalculate width on window resize */
+    $(window).on('resize', updateWidth);
 });
+
